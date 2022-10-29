586e291970765573111f2fb79ad714899ebf558d
Mohd-ismail0 <67965009+Mohd-ismail0@users.noreply.github.com>
2022-10-30 03:03:37 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,69 +1,45 @@
-// var i = 0;
-// //var txt = "Hello, my name is Mohammed Ismail. I am a generalist with a broad range of skills, including Python, SQL, C#, HTML-CSS-JS, Flutter and UI/UX Design."; /* The text */
-// var txt = ["Python", "SQL", "C", "HTML-CSS-JS", "Flutter and UI/UX Design"];
-// var speed = 65; /* The speed/duration of the effect in milliseconds */
-
-// function typeWriter() {
-//   for (i=0; i<txt.length; i++) {
-//     let str=txt[i];
-//     let a=str.length;
-//     for (let j = 0; j <= a; j++) {
-//       if (j < (a-1)) {
-//         document.getElementById("demo").innerHTML += str[j];
-//         setTimeout(typeWriter, speed);
-//       }
-//       else if(j == (a-1)) {
-//         document.getElementById("demo").innerHTML += str[j];
-//         setTimeout(typeWriter, 18000);
-//       }
-//       else if(j==a){
-//         document.getElementById("demo").innerHTML ="";
-//         setTimeout(typeWriter, speed);
-//       }
-//     }
-// }
-// }
-
-var TxtType = function(el, toRotate, period) {
-  this.toRotate = toRotate;
-  this.el = el;
-  this.loopNum = 0;
-  this.period = parseInt(period, 10) || 2000;
-  this.txt = '';
-  this.tick();
-  this.isDeleting = false;
-};
-
-TxtType.prototype.tick = function() {
-  var i = this.loopNum % this.toRotate.length;
-  var fullTxt = this.toRotate[i];
-
-  if (this.isDeleting) {
-  this.txt = fullTxt.substring(0, this.txt.length - 1);
-  } else {
-  this.txt = fullTxt.substring(0, this.txt.length + 1);
+class TxtType {
+  constructor(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
   }
+  tick() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
 
-  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
 
-  var that = this;
-  var delta = 200 - Math.random() * 100;
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
 
-  if (this.isDeleting) { delta /= 2; }
+    var that = this;
+    var delta = 200 - Math.random() * 100;
 
-  if (!this.isDeleting && this.txt === fullTxt) {
-  delta = this.period;
-  this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === '') {
-  this.isDeleting = false;
-  this.loopNum++;
-  delta = 500;
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+    }
+
+    setTimeout(function () {
+      that.tick();
+    }, delta);
   }
+}
 
-  setTimeout(function() {
-  that.tick();
-  }, delta);
-};
 
 window.onload = function() {
   var elements = document.getElementsByClassName('typewrite');
@@ -83,3 +59,12 @@ window.onload = function() {
 // var con=document.getElementById("con");
 // con.addEventListener("click",notavl);
 // res.addEventListener("click",notavl);
+/* 
+function myFunction1() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+} */
